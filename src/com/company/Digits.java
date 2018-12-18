bdb1d4220afa3cd5d09246c862480896698c1579
BT_1N3_03 <sli7236@bths.edu>
2018-12-18 11:14:57 -0500

    string

--- src/com/company/Digits.java
+++ src/com/company/Digits.java
@@ -5,6 +5,7 @@ public class Digits {
 
     private ArrayList<Integer> digitList;
 
+    //int method
     public Digits(int num)
     {
         digitList = new ArrayList<Integer>();
@@ -31,6 +32,26 @@ public class Digits {
         }
         return true;
     }
+
+    // string method
+    /*public Digits(int num) {
+        digitList = new ArrayList<Integer>();
+        //Part A
+        String input = num + ""; //cast to string
+        for (int i = 0; i < input.length(); i++) {
+            digitList.add(Integer.parseInt(input.substring(i, i+1)));
+        }
+    }
+
+    public boolean isStrictlyIncreasing() {
+        //Part B
+        for (int i = 0; i < digitList.size()-1; i++) {
+            if (digitList.get(i) >= digitList.get(i+1)) {
+                return false;
+            }
+        }
+        return true;
+    }*/
 
     public ArrayList<Integer> getDigitList()
     {
